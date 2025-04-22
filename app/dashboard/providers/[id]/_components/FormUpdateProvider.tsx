086f1caaab4f271762e1a592d559e155007862f6
Demonik-Argo23 <cerdivaca9@gmail.com>
2025-04-21 20:31:34 -0600

    feat: Providers Finished

--- app/dashboard/providers/[id]/_components/FormUpdateProvider.tsx
+++ app/dashboard/providers/[id]/_components/FormUpdateProvider.tsx
@@ -1,6 +1,8 @@
 import updateProvider from "@/actions/providers/update";
 import { Provider } from "@/entities";
 import { Button, Input } from "@heroui/react";
+import DeleteProvider from "./DeleteProvider";
+import DeleteProviderButton from "./DeleteButton";
 
 export default function FormUpdateProvider({ provider }: { provider: Provider }) {
     const { providerId } = provider;
@@ -15,6 +17,10 @@ export default function FormUpdateProvider({ provider }: { provider: Provider })
                 <Input className="max-w-[250px]" defaultValue={provider.providerEmail} label="Correo" name="providerEmail" />
                 <Input className="max-w-[250px]" defaultValue={provider.providerPhoneNumber} label="Número" name="providerPhoneNumber" />
                 <Button type="submit" color="primary" className="mt-4"> Actualizar Proveedor </Button>
+                <DeleteProvider>
+                    <h1 className="text-white text-3x1 text-center"> ¿Estás seguro de eliminar al proveedor <b>{provider.providerName}</b>? </h1>
+                    <DeleteProviderButton providerId={providerId} />
+                </DeleteProvider>
             </form>
         </>
     )
